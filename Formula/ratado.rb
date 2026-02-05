2012f60478298440e3148ff6bcbc42e2e5e3a3dd
Miloš Mirić <479077+milosmiric@users.noreply.github.com>
2026-02-05 13:00:05 +0100

    Update ratado to v0.2.0

--- Formula/ratado.rb
+++ Formula/ratado.rb
@@ -1,23 +1,23 @@
 class Ratado < Formula
   desc "A fast, keyboard-driven terminal task manager built with Rust and Ratatui"
   homepage "https://github.com/milosmiric/ratado"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/milosmiric/ratado/releases/download/v0.1.0/ratado-aarch64-apple-darwin.tar.gz"
-      sha256 "521636105389a376a8e2763d70e656456439fc1aa0f7130094a1672444d599c2"
+      url "https://github.com/milosmiric/ratado/releases/download/v0.2.0/ratado-aarch64-apple-darwin.tar.gz"
+      sha256 "7722071b2072ba4c939f726f09dcaf92e272479bb2c5a640531bcacae66007f1"
     end
     on_intel do
-      url "https://github.com/milosmiric/ratado/releases/download/v0.1.0/ratado-x86_64-apple-darwin.tar.gz"
-      sha256 "eb03e2e75485ed8e0a4e6d25f6ff4b27a5813c6c1065135272a7f7d5da3a30e7"
+      url "https://github.com/milosmiric/ratado/releases/download/v0.2.0/ratado-x86_64-apple-darwin.tar.gz"
+      sha256 "2571bf3243bc3953e3b782fc6064256c30790ce3e73c9017b4634625cd5fd51e"
     end
   end
 
   on_linux do
-    url "https://github.com/milosmiric/ratado/releases/download/v0.1.0/ratado-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "cca94da880720fd071805546df8c44c6db72584fd1cecf321b86d9698fb2542c"
+    url "https://github.com/milosmiric/ratado/releases/download/v0.2.0/ratado-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "d380653b5c3726c11606db842a1e461147116b79a325af75d659fcaa7e4d6b1f"
   end
 
   def install
